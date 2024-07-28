3e1072cb1577fd98ee2891e7d1b42ae13214520b
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2024-07-28 21:38:11 +0100

    feat: all elements ready
    Coloquei todos os elementos no frontend. Agora, falta adicionar as funcionalidade

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { MapPin, Calendar, ArrowRight } from 'lucide-react'
 export function App() {
 
   return (
-   <div className="h-screen flex items-center justify-center">
+   <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
 
-        <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+        <div className='flex flex-col items-center gap-3'>
+          <img src="./logo.svg" alt="plann.er" />
+          <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+        </div>
         
         <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
           <div className='flex items-center gap-2 flex-1'>
             <MapPin className='text-zinc-300 size-5' />
-            <input type="text" placeholder="Para onde você vai?" className='bg-transparent text-lg placeholder-zinc-400 outline-none'/>
+            <input type="text" placeholder="Para onde você vai?" className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1'/>
           </div>
           
           <div className='flex items-center gap-2'>
