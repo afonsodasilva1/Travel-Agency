4b5d556d6d2e7871528baa2b05de0fd12acd4b34
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2024-07-28 21:00:41 +0100

    feat: Add bg image

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,12 @@ export function App() {
         <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
           <div className='flex items-center gap-2 flex-1'>
             <MapPin className='text-zinc-300 size-5' />
-            <input type="text" placeholder="Para onde você vai?" className='bg-transparent text-lg placeholder-zinc-400'/>
+            <input type="text" placeholder="Para onde você vai?" className='bg-transparent text-lg placeholder-zinc-400 outline-none'/>
           </div>
           
           <div className='flex items-center gap-2'>
             <Calendar className='size-5 text-zinc-300' />
-            <input type="text" placeholder="Quando?" className='bg-transparent text-lg placeholder-zinc-400' />
+            <input type="text" placeholder="Quando?" className='bg-transparent text-lg placeholder-zinc-400 w-40 outline-none' />
           </div>
 
           <div className='w-px h-6 bg-zinc-800' />
