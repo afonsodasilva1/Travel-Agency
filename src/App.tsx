c7a0e79079ce648edc6380ad5490e610120751ef
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2024-07-28 17:00:30 +0100

    feat: Finish all css treatments

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { MapPin, Calendar, ArrowRight } from 'lucide-react'
+
 export function App() {
 
   return (
@@ -6,12 +8,23 @@ export function App() {
 
         <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
         
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center">
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+          <div className='flex items-center gap-2 flex-1'>
+            <MapPin className='text-zinc-300 size-5' />
+            <input type="text" placeholder="Para onde você vai?" className='bg-transparent text-lg placeholder-zinc-400'/>
+          </div>
+          
+          <div className='flex items-center gap-2'>
+            <Calendar className='size-5 text-zinc-300' />
+            <input type="text" placeholder="Quando?" className='bg-transparent text-lg placeholder-zinc-400' />
+          </div>
 
-          <input type="text" placeholder="Para onde você vai?" />
-          <input type="date" placeholder="Quando?" />
-
-          <button>Continuar</button>
+          <div className='w-px h-6 bg-zinc-800' />
+          
+          <button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-500'>
+            Continuar
+            <ArrowRight className='size-5'/>
+          </button>
         </div>
 
         <p className="text-sm text-zinc-500">
