164f43cae4c78deedc73bf02d7d96b26c4672211
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2024-07-29 20:08:03 +0100

    feat (App.tsx): Add a new feature isGuestsInputOpen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import { MapPin, Calendar, ArrowRight } from 'lucide-react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus } from 'lucide-react'
+import { useState } from 'react'
 
 export function App() {
+
+  const [ isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
+
+  function openGuestsInput () {
+
+    setIsGuestsInputOpen(true)
+
+    return setIsGuestsInputOpen
+  }
 
   return (
    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
@@ -10,24 +20,42 @@ export function App() {
           <img src="./logo.svg" alt="plann.er" />
           <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
         </div>
-        
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-          <div className='flex items-center gap-2 flex-1'>
-            <MapPin className='text-zinc-300 size-5' />
-            <input type="text" placeholder="Para onde você vai?" className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1'/>
-          </div>
-          
-          <div className='flex items-center gap-2'>
-            <Calendar className='size-5 text-zinc-300' />
-            <input type="text" placeholder="Quando?" className='bg-transparent text-lg placeholder-zinc-400 w-40 outline-none' />
+
+      <div className='flex flex-col gap-2'>  
+          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+            <div className='flex items-center gap-4 flex-1'>
+              <MapPin className='text-zinc-300 size-5' />
+              <input type="text" placeholder="Para onde você vai?" className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1'/>
+            </div>
+            
+            <div className='flex items-center gap-2'>
+              <Calendar className='size-5 text-zinc-300' />
+              <input type="text" placeholder="Quando?" className='bg-transparent text-lg placeholder-zinc-400 w-40 outline-none' />
+            </div>
+
+            <div className='w-px h-6 bg-zinc-800' />
+            
+            <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-500'>
+              Continuar
+              <ArrowRight className='size-5'/>
+            </button>
           </div>
 
-          <div className='w-px h-6 bg-zinc-800' />
-          
-          <button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-500'>
-            Continuar
-            <ArrowRight className='size-5'/>
-          </button>
+          { isGuestsInputOpen && (
+            <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+              <div className='flex items-center gap-2 flex-1'>
+                <UserRoundPlus className='text-zinc-300 size-5' />
+                <input type="text" placeholder="Quem estará na viagem?" className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1'/>
+              </div>
+
+              <div className='w-px h-6 bg-zinc-800' />
+              
+              <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-500'>
+                Continuar
+                <ArrowRight className='size-5'/>
+              </button>
+          </div>
+          )}
         </div>
 
         <p className="text-sm text-zinc-500">
